a01c6e6c40c0508a4a01ea329bc2d695e852f424
breadbutterjam <jaimeen@gmail.com>
2019-07-10 16:03:35 +0530

    Create hyperlinks for all todos in the todo list

---
+++ scripts/copybasecamptodos_createHyperlinkS.js
@@ -0,0 +1,24 @@
+function SlayIt()
+{
+strCopy = "";
+a = document.getElementsByClassName('todos')[1].children
+data = [];
+for (i=0; i<a.length; i++)
+{
+	elem = a[i].children[1].children[0].children[1].children[0];
+	todotitle = elem.textContent.trim();
+	todolink = elem.href;
+	data.push(todotitle)
+	data.push(todolink)
+    // strCopy += todotitle + '\n' + todolink + '\n';
+    // let strToCopy3 = '=HYPERLINK("' + strToCopy2 + '","' + strToCopy1 +'")';
+	strCopy += '=HYPERLINK("' + todolink + '","' + todotitle +'")' + '\n';
+}
+
+let x = document.createElement("TEXTAREA");
+x.value = strCopy
+document.body.append(x);
+x.select()
+document.execCommand("copy");
+x.remove();
+}SlayIt();
