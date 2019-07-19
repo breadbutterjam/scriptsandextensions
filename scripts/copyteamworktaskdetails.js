1926cc01ce59d14c89a4b1b5060b4031545b58df
breadbutterjam <jaimeen@gmail.com>
2019-07-19 17:21:14 +0530

    Parametric return hyperlink or normal text

--- scripts/copyteamworktaskdetails.js
+++ scripts/copyteamworktaskdetails.js
@@ -1,13 +1,28 @@
-function slayit()
+function slayit(createHyperlink)
 {
+   if (createHyperlink === undefined)
+      createHyperlink = true;
+   // createHyperlink ? undefined : true
+
+
    let strToCopy1 = document.querySelectorAll('.task-name')[0].children[1].textContent.trim();
    let strToCopy2 = window.location.href;
    let strToCopy3 = '=HYPERLINK("' + strToCopy2 + '","' + strToCopy1 +'")';
    let x = document.createElement("TEXTAREA");
    // x.value = strToCopy1 + '\n' + strToCopy2 + '\n' + strToCopy3;
-   x.value = strToCopy3;
+   if (createHyperlink)
+   {
+      x.value = strToCopy3;
+   }
+   else
+   {
+      x.value = strToCopy1 + '\n' + strToCopy2 + '\n' + strToCopy3;
+   }
+
+
+
    document.body.append(x);
    x.select()
    document.execCommand("copy");
    x.remove();
-}slayit();
+}slayit(false);
