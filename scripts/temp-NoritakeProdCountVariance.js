ed98eea54828947f5e157d74ec424e36bc1c2ccb
breadbutterjam <jaimeen@gmail.com>
2019-08-20 16:33:13 +0530

    Update

--- scripts/temp-NoritakeProdCountVariance.js
+++ scripts/temp-NoritakeProdCountVariance.js
@@ -3,3 +3,12 @@
 
 //
 //bb = "5811::20-Piece Set,924::4-Piece Coupe Place Setting,6664::4-Piece Curve Place Setting,925::4-Piece Rim Place Setting,926::4-Piece Square Place Setting,5555::5-Piece Place Setting,928::Accent/Luncheon Plate, Floral, 8 1/4" (Cornflower),934::Bakeware, Covered Casserole, 2 qt.,936::Bakeware, Ramekins, Set of 2, 3 3/4", 9 oz.,935::Bakeware, Round Baker/Pie Plate, 9 1/2",5591::Bowl, Coupe Pasta, 35 oz., 9 1/4",937::Bowl, Handled, 5 1/2" 18 oz.,5195::Bowl, Large Mixing, 10", 112 oz.,938::Bowl, Large Round Vegetable, 9 1/2", 64 oz.,5680::Bowl, Large Square, 10", 46 oz.,5194::Bowl, Medium Mixing, 8", 58 oz.,5679::Bowl, Medium Square, 7 3/4", 18 oz.,939::Bowl, Mini, 4", 7 oz,6266::Bowl, Mini, Set of 4, 4", 7 oz,940::Bowl, Pasta Serving, 12", 89 1/2 oz. (3 qt),941::Bowl, Pasta, 10 1/2", 27 oz.,942::Bowl, Pasta/Rim Soup, 8 1/2", 20 oz.,943::Bowl, Rice, 5 3/4", 20 oz.,4442::Bowl, Side / Prep, 5", 12 oz.,5193::Bowl, Small Mixing, 6 1/4", 27 oz.,945::Bowl, Soup/Cereal, 7", 22 oz.,6667::Bowl, Soup/Cereal, Curve, 7", 22 oz.,946::Bowls, Pasta/Rim Soup, Set of 4, 8 1/2", 20 oz.,944::Bowls, Set of 3,947::Chip & Dip, 14 3/4",6662::Colorwave 20th Anniversary Apron,6217::Colorwave Blue Cloth Napkin, Set of 2,6229::Colorwave Blue Placemat,6207::Colorwave Blue Tablecloth,948::Covered Butter,6658::Covered Casserole LID ONLY,949::Creamer, 11 oz.,5594::Crescent Tray, 9 3/4",951::Dinner Plate, Coupe, 10 1/2",6666::Dinner Plate, Curve, 10 1/2",952::Dinner Plate, Rim, 11",953::Dinner Plate, Square, 10 3/4",954::Gravy, 20 oz.,955::Mini Plate, 6 1/4",6033::Mini Plate, 6 1/4" (Set of 4),956::Mug, 12 oz.,5593::Oblong Tray, Large, 16" x 3 1/2",5592::Oblong Tray, Small, 9" x 2 3/4",957::Pitcher, 60 oz.,958::Platter, Coupe Round, 12",959::Platter, Oval, 16",960::Platter, Round Rim, 12 1/2",3487::Platter, Square 11 3/4",961::Quad Plate, Large, 11 3/4",962::Quad Plate, Medium, 10 3/4",963::Quad Plate, Mini, 6 1/2",4197::Quad Plate, Mini, Set of 4, 6 1/2",964::Quad Plate, Small, 8 1/4",967::Salad/Dessert Plate, Coupe, 8 1/4",6665::Salad/Dessert Plate, Curve, 8 1/4",965::Salad/Dessert Plate, Rim, 8 1/4",966::Salad/Dessert Plate, Square, 8 1/4",968::Salt & Pepper, 3 3/8",4443::Side Prep Dish, Set of 8, 4 1/2",4911::Small Teapot, 24 oz.,970::Sugar with Cover, 13 oz.,4910::Tea for Two,5281::Tumbler, 12 oz., Set of Four,5284::Tumbler, 12 oz., Set of Four,3486::Two-Tiered Tray"
+
+
+a = Array.from(document.querySelectorAll('.jam li .item-name'))
+b = [];
+a.forEach(function(ele){
+	b.push(ele.textContent.trim())
+})
+
+
